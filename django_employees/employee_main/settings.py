8c036d136f7ad33bf98002443bea8ea6a8bacd5a
Daniel <danielcoellovargas@gmail.com>
2025-11-26 21:02:28 -0500

    Fix static files directory path in settings
    Corrected STATICFILES_DIRS from 'employees_main/static' to 'employee_main/static' to match the actual app directory structure. (This fixed the broken CSS)

--- django_employees/employee_main/settings.py
+++ django_employees/employee_main/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'staticfiles'
-STATICFILES_DIRS = ['employees_main/static']
+STATICFILES_DIRS = ['employee_main/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
