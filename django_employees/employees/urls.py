49de5192f660dde9f518f7009ec740097783060c
Daniel <danielcoellovargas@gmail.com>
2025-12-01 20:29:02 -0500

    Add employee creation form and view
    Introduced EmployeeForm for model-based employee creation. Added add_employee view and template to allow users to add new employees via a form. Updated URLs and home page to link to the new add employee functionality.

--- django_employees/employees/urls.py
+++ django_employees/employees/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>/', views.employee_detail, name ='employee_detail')
+    path('<int:id>/', views.employee_detail, name ='employee_detail'),
+    path('add/', views.add_employee, name = 'add_employee'),
+
 ]
--- django_employees/employees/views.py
+++ django_employees/employees/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Employee
+from .forms import EmployeeForm
 
 # Decide with url pattern to create
 # created url patter in the main urls. and forward it to emlpoyees.urls
@@ -13,3 +14,17 @@ def employee_detail(request, id):
         'employee':employee
     }
     return render(request, 'employee_detail.html', context)
+
+
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.errors)
+    form = EmployeeForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'add_employee.html', context)
